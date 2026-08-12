8b97d0d9f6c3d862a062b9ae5a635fa31aa99e9d
ryunsong-contentful <ryun.song@contentful.com>
2026-08-12 12:08:28 -0600

    feat: add Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero'
 import SeasonDetails from './components/SeasonDetails'
 import Pricing from './components/Pricing'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Hero />
       <SeasonDetails />
       <Pricing />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>Questions? Email <a href="mailto:walkonsocial@gmail.com">walkonsocial@gmail.com</a></p>
+      <p>Free agent? We'll place you on a team.</p>
+    </footer>
+  )
+}
